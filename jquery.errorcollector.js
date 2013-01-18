e1baa4476fac4e52a62c2d88e7e7b9ca95d38839
Mikhail <mihdan@yandex.ru>
2013-01-18 15:18:33 +0400

    Update jquery.errorcollector.js

--- jquery.errorcollector.js
+++ jquery.errorcollector.js
@@ -2,9 +2,23 @@
     
     var defaults = {
         text: 'Сообщение успешно отправлено'
-    };     
+    };
     
     var options;
+    
+    var methods = {
+        getSelectedText : function( options ) { 
+            var selectedText;
+            if (window.getSelection) {
+                selectedText = window.getSelection();
+			} else if (document.getSelection) {
+                selectedText = document.getSelection();
+            } else if (document.selection) {
+                selectedText = document.selection.createRange().text;
+            }
+        }
+   
+    };
      
     $.fn.errorCollector = function(params){
         
